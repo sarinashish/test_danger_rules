93416cb10ff2ed7fc75333f814be256f472086ac
sarinashish <ashish.sarin@getfareye.com>
2023-01-20 23:27:17 +0530

    sdsd

---
+++ New.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+let dummay = "dummy2"
+export default NewComponent extends React.Component {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
